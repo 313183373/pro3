ee25b40d4dedb402d8fcb75abc7ac536f2cd6c9f
star xiong <313183373@qq.com>
2017-05-28 11:13:53 +0800

    haha

--- main/main.js
+++ main/main.js
@@ -1,3 +1,8 @@
-module.exports = function main() {
+module.exports = function main(a) {
   // Write your cade here
+  var sum=0;
+  for(let i=0;i<a.length;i++){
+  	sum+=parseInt(a.charAt(i));
+  }
+  return sum;
 };
